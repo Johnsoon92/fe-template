2705eb3a347962b759d390d8e608bc74c199b54a
SeanJohn <258514255@qq.com>
2018-05-02 21:10:09 +0800

    and some config

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 
 module.exports = {
@@ -14,6 +16,10 @@ module.exports = {
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'fe-template'
+    }),
+    new VueLoaderPlugin()，
+    new MiniCssExtractPlugin({
+      filename: style.css
     })
   ],
   output: {
@@ -25,11 +31,39 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: file => (
+          /node_modules/.test(file) &&
+          !/\.vue\.js/.test(file)
+        )
+      },
+      {
         test: /\.css$/,
         use: [
-          'style-loader',
+          process.env.NODE_ENV !== 'production'
+            ? 'vue-style-loader'
+            : MiniCssExtractPlugin.loader,
           'css-loader'
           ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              // you can also read from a file, e.g. `variables.scss`
+              data: `variables.scss`
+            }
+          }
+        ]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
